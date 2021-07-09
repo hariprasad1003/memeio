820aa7ce88f70719fa3f713f1dec132354db6747
hariprasad1003 <hariprasadgurunathan10@gmail.com>
2021-07-09 22:04:03 +0530

    create room page added and home page modified

--- app.py
+++ app.py
@@ -32,10 +32,24 @@ users  = mongo.db.cio_users
 
 participants=[]
 
+'''
+http://127.0.0.1:5000/
+'''
+
 @app.route("/", methods = ['GET', 'POST'])
-def index():
-    # return render_template("home.html")
-    return redirect(url_for("login_get"))
+def home():
+
+    if(request.method=='POST'):
+
+        room_pin = request.form['room_pin']
+
+        # print(room_pin)
+
+    return render_template("home.html")
+
+'''
+http://127.0.0.1:5000/login
+'''
 
 @app.route("/login", methods = ['GET'])
 def login_get():
@@ -57,6 +71,10 @@ def login_post():
 
         return render_template("login.html", error_message = error_message)
 
+'''
+http://127.0.0.1:5000/signup
+'''
+
 @app.route("/signup", methods = ['GET'])
 def signup_get():
     return render_template("signup.html")
@@ -73,6 +91,29 @@ def signup_post():
         business.signup(email, username, password)
 
     return render_template("home.html")
+
+'''
+http://127.0.0.1:5000/create/room
+'''
+
+@app.route("/create/room", methods = ['GET'])
+def get_create_room():
+    return render_template("create_room.html")
+
+@app.route("/create/room", methods = ['POST'])
+def post_create_room():
+
+    if(request.method=='POST'):
+
+        room_name = request.form['room_name']
+
+        # print(room_name)
+
+    return render_template("create_room.html")
+
+'''
+http://127.0.0.1:5000/room
+'''
 
 @app.route("/room", methods = ['GET', 'POST'])
 def room():
