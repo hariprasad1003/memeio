0480f2e5d068cb930936c6bf7a54d734001994f6
hariprasad1003 <hariprasadgurunathan10@gmail.com>
2022-01-02 10:10:34 +0530

    server, image resize and button issues fixed

--- app.py
+++ app.py
@@ -326,4 +326,5 @@ def leave(data):
     emit('status', result_dict )
 
 if __name__ == '__main__':
-    socketio.run(app, host="0.0.0.0", debug=True)
+    socketio.run(app, debug=True)
+    # socketio.run(app, host="0.0.0.0", debug=True)
